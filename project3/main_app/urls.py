3af4d07985ace518eb4447e3177fffbf324d014c
txbell <tristan@bellevin.com>
2023-04-03 11:34:19 -0700

    added urls and views

--- project3/main_app/urls.py
+++ project3/main_app/urls.py
@@ -2,5 +2,17 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    # home paths
     path('', views.home, name='home'),
+    path('about/', views.about, name='about'),
+    # user paths
+        # for now when you look at user it starts off showing past activities
+    path('user/<int:user_id>/', views.past_activities, name='past_activities'),
+        # this is the path to the user's interests
+    path('user/<int:user_id>/interests/', views.interests, name='interests'),
+        # bcs interests is an array of the user, we just need to update the user model
+    path('user/<int:user_id>/interests/edit/', views.UserUpdate.as_view(), name='edit_interest'),
+    # recommend path
+        # this is the path to the recommendation page
+    path('user/<int:user_id>/recommend/', views.recommend, name='recommend'),
 ]
--- project3/main_app/views.py
+++ project3/main_app/views.py
@@ -1,7 +1,44 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.views.generic import ListView, DetailView
+from .models import User, Activity, UserActivity
 
 # Create your views here.
 
 # Define the home view
 def home(request):
   return render(request, 'welcome.html')
+
+# Define the about view
+def about(request):
+  return render(request, 'about.html')
+
+# Define the past_activities view
+def past_activities(request, user_id):
+  user = User.objects.get(id=user_id)
+  past_activities = UserActivity.objects.filter(user=user)
+  return render(request, 'past_activities.html', {
+    'user': user,
+    'past_activities': past_activities
+  })
+
+# Define the interests view
+def interests(request, user_id):
+  user = User.objects.get(id=user_id)
+  interests = User.objects.get(id=user_id).interests
+  return render(request, 'interests.html', {
+    'user': user,
+    'interests': interests
+  })
+
+class UserUpdate(UpdateView):
+  model = User
+  fields = ['interests']
+  success_url = '/user/<int:user_id>/interests/'
+
+# Define the recommend view
+def recommend(request, user_id):
+  user = User.objects.get(id=user_id)
+  return render(request, 'recommend.html', {
+    'user': user
+  })
