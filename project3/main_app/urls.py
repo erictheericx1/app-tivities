5760cb51f1424f4268b45a48ff77cb3b5e486a66
txbell <tristan@bellevin.com>
2023-04-04 18:02:12 -0700

    activity CRUD paths setup need templates

--- project3/main_app/urls.py
+++ project3/main_app/urls.py
@@ -12,13 +12,26 @@ urlpatterns = [
         # this is the path to the user's interests
     path('user/<int:user_id>/interests/', views.interests, name='interests'),
         # bcs interests is an array of the user, we just need to update the user model
-    path('user/<int:user_id>/interests/edit/', views.UserUpdate.as_view(), name='edit_interest'),
+    path('user/<int:pk>/interests/edit/', views.AppUserUpdate.as_view(), name='edit_interest'),
+
+    path('user/<int:user_id>/create/', views.UserCreate.as_view(), name='create_appuser'),
     # recommend path
         # this is the path to the recommendation page
     path('user/<int:user_id>/recommend/', views.recommend, name='recommend'),
+
+    path('user/<int:user_id>/recommend/<int:activity_id>/', views.add_activity, name='add_activity'),
+
     path('accounts/', include('django.contrib.auth.urls')),
         # localhost:8000/accounts/login 
         # built in django user authentication
     # path('user/<int:user_id>/past_activities/', views.past_activities.as_view(), name='past_activities'),
-]
+    path('accounts/signup/', views.signup, name='signup'),
 
+    #activity paths
+    path('activity/', views.ActivityList.as_view(), name='activity'),
+    path('activity/<int:pk>/', views.ActivityDetail.as_view(), name='activity_detail'),
+    path('activity/create/', views.ActivityCreate.as_view(), name='activity_create'),
+    path('activity/<int:pk>/update/', views.ActivityUpdate.as_view(), name='activity_update'),
+    path('activity/<int:pk>/delete/', views.ActivityDelete.as_view(), name='activity_delete'),
+
+]
--- project3/main_app/views.py
+++ project3/main_app/views.py
@@ -2,6 +2,10 @@ from django.shortcuts import render, redirect
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.views.generic import ListView, DetailView
 from .models import User, Activity, UserActivity, AppUser
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 # Create your views here.
 
@@ -14,6 +18,7 @@ def about(request):
   return render(request, 'about.html')
 
 # Define the past_activities view
+@login_required
 def past_activities(request, user_id):
   user = User.objects.get(id=user_id)
   past_activities = UserActivity.objects.filter(user_id=user_id)
@@ -26,6 +31,7 @@ def past_activities(request, user_id):
   })
 
 # Define the interests view
+@login_required
 def interests(request, user_id):
   user = User.objects.get(id=user_id)
   appuser = AppUser.objects.get(id=user_id)
@@ -33,22 +39,24 @@ def interests(request, user_id):
     'user': user,
     'appuser': appuser,
   })
+##not working on new logins? maybe because of the user_id?
 
-class UserUpdate(UpdateView):
+class AppUserUpdate(UpdateView):
   model = AppUser
   fields = ['interests']
-  success_url = '/user/<int:user_id>/interests/'
+  success_url = '/user/{user_id}/interests/'
 
-class UserCreate(CreateView):
+class UserCreate(LoginRequiredMixin, CreateView):
   model = AppUser
   fields = ['interests']
-  success_url = '/user/<int:user_id>/interests/'
+  success_url = '/user/{user.id}/interests/'
 
   def form_valid(self, form):
     form.instance.user = self.request.user
     return super().form_valid(form)
 
 # Define the recommend view
+@login_required
 def recommend(request, user_id):
   user = User.objects.get(id=user_id)
   appuser = AppUser.objects.get(id=user_id)
@@ -59,11 +67,56 @@ def recommend(request, user_id):
     'recommendation': recommendation,
   })
 
-class UserActivityCreate(CreateView):
-  model = UserActivity
-  fields = ['activity']
-  success_url = '/user/<int:user_id>/past_activities/'
+# Logging user did activity
+# class UserActivityCreate(CreateView):
+#   model = UserActivity
+#   fields = ['activity']
+#   success_url = '/user/<int:user_id>/past_activities/'
 
-  def form_valid(self, form):
-    form.instance.user = AppUser.objects.get(id=self.request.user.id)
-    return super().form_valid(form)
+#   def form_valid(self, form):
+#     form.instance.user = AppUser.objects.get(id=self.request.user.id)
+#     return super().form_valid(form)
+
+@login_required
+def add_activity(request, user_id, activity_id):
+  UserActivity.objects.create(user=AppUser.objects.get(id=user_id), activity=Activity.objects.get(id=activity_id))
+  print('activity added')
+  return redirect('past_activities', user_id=user_id)
+
+
+# signup view
+def signup(request):
+  error_message = ''
+  if request.method == 'POST':
+    form = UserCreationForm(request.POST)
+    if form.is_valid():
+      user = form.save()
+      app_user = AppUser.objects.create(user=user)
+      app_user.save()
+      login(request, user)
+      return redirect('edit_interests', user_id=user.id)
+      # return redirect('recommend')
+    else:
+      error_message = 'Invalid sign up - try again'
+  form = UserCreationForm()
+  context = {'form': form, 'error_message': error_message}
+  return render(request, 'registration/signup.html', context)
+
+class ActivityCreate(LoginRequiredMixin, CreateView):
+  model = Activity
+  fields = '__all__'
+  success_url = '/activities/'
+
+class ActivityUpdate(LoginRequiredMixin, UpdateView):
+  model = Activity
+  fields = ['name', 'description', 'interests']
+
+class ActivityDelete(LoginRequiredMixin, DeleteView):
+  model = Activity
+  success_url = '/activities/'
+
+class ActivityList(LoginRequiredMixin, ListView):
+  model = Activity
+
+class ActivityDetail(LoginRequiredMixin, DetailView):
+  model = Activity
