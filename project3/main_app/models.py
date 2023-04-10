eaaa48fb9c8e25992fa36c2d1203e61064eded95
ellereyy <elle.m.reynolds@gmail.com>
2023-04-10 02:48:47 -0700

    attempted to fix migrations and cleaned up styles

--- project3/main_app/models.py
+++ project3/main_app/models.py
@@ -2,7 +2,9 @@ from django.db import models
 from django.contrib.postgres import fields as arrayField
 from django.contrib.auth.models import User
 
-default_interests = ('Art', 'Music', 'Sightseeing', 'Shopping', 'Restaurants', 'Bars', 'Events', 'Outdoor Recreation', 'History', 'Nature', 'Education', 'Fitness', 'Travel', 'Books', 'Photography', 'Theater', 'Architecture', 'Cuisine', 'Local Culture', 'Street Markets', 'Museums', 'Urban Exploration', 'City Parks', 'Public Transportation')
+# default_interests = ('Art', 'Music', 'Sightseeing', 'Shopping', 'Restaurants', 'Bars', 'Events', 'Outdoor Recreation', 'History', 'Nature', 'Education', 'Fitness', 'Travel', 'Books', 'Photography', 'Theater', 'Architecture', 'Cuisine', 'Local Culture', 'Street Markets', 'Museums', 'Urban Exploration', 'City Parks', 'Public Transportation')
+
+default_interests = ('Art', 'Music', 'Sports', 'Games', 'Food', 'Travel', 'Fashion', 'Technology', 'Science', 'Nature', 'Politics', 'Religion', 'History', 'Education', 'Health', 'Fitness', 'Space', 'Cars', 'Bars', 'Photography', 'Painting', 'Writing', 'Reading')
 
 # Create your models here.
 class AppUser(models.Model):
